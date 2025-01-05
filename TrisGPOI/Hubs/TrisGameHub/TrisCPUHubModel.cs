1d26bce2a0aa8e3b4bcf88eaffe80158138ccebc
hkj123 <134635067+hkj1234@users.noreply.github.com>
2025-01-05 16:17:53 +0100

    Testing

--- TrisGPOI/Hubs/TrisGameHub/TrisCPUHubModel.cs
+++ TrisGPOI/Hubs/TrisGameHub/TrisCPUHubModel.cs
@@ -13,9 +13,6 @@ namespace TrisGPOI.Hubs.TrisGameHub
 
         public override async Task OnConnectedAsync()
         {
-            //da fare, da togliere
-            await Clients.Client(Context.ConnectionId).SendAsync("ReceiveMove", "ciao");
-
             var email = Context.User?.Identity?.Name;
             try
             {
